f4dcba3951daed3641ca84c5a2c582fbe53e1c37
Dmitry Leonov <dleonov@dev.football.com>
2016-08-11 18:08:15 +0300

    second

--- app/test2/views/query.js
+++ app/test2/views/query.js
@@ -7,16 +7,28 @@ module.exports = Marionette.ItemView.extend({
 
   template: template,
 
+  xhr: null,
+
   events: {
     'keyup [name=query]': 'onQueryKeyup',
   },
 
+  initialize: function() {
+    this.collection = this.model.getGenres();
+  },
+
   onQueryKeyup: function(e) {
     var query = e.currentTarget.value;
+
+    if (!query.length) this.collection.reset();
+
     this.model.set('id', query);
-    this.model.fetch({
+
+    this.xhr && this.xhr.abort();
+    this.xhr = this.model.fetch({
       headers: { Authorization: 'bearer client'}
     });
+
   }
 
 });
